c36d5b530ea0a0a132120d385cb87f80c0c36408
FullstackDeveshPanday <deveshpandeyy007@gmail.com>
2021-01-18 22:57:27 +0530

    todolist-app-React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import React,{useState} from 'react';
 import './App.css';
+// importing components
+import Form from './components/Form.js';
+import TodoList from './components/TodoList.js';
 
 function App() {
+
+  const[inputText ,setText]=useState("");
+  const[todo,todos]=useState([]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <h1>To-Do List</h1>
       </header>
+      <Form setText={setText}/> 
+      <TodoList/>  
     </div>
   );
 }
